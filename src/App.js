6de23e0d5c1ff19858b4afe0d61a5fbf2dcce06d
Pau Arjalaguer <pau.arjalaguer@gmail.com>
2024-05-09 16:52:16 +0200

    Video Web

--- src/App.js
+++ src/App.js
@@ -10,23 +10,28 @@ function App() {
     <>
       <video autoPlay muted loop id="background-video" className='background'>
         <source src="assets/images/video.mp4" type="video/mp4" />
-      </video> <div className='flex justify-center mt-20'>
-        <h1 className='font-bold text-slate-700 text-4xl'>Pau Arjalaguer</h1>
+      </video>
+      <div className='absolute top-0 flex flex-col w-full justify-center items-center mt-20 '>
+        <h1 className='font-bold text-slate-700 text-4xl'>Pau Arjalaguer</h1><small className='text-2xl'>Projectes destacats</small>
       </div>
-      <Divz autoPlay={true}
-        autoPlayDuration={10000}>
+      <div className='flex justify-center  border-2'>
+        <Divz autoPlay={true}
+          autoPlayDuration={3000}>
+          <div>
+            <video autoPlay muted loop onClick={() => { goToUrl("https://apps.apple.com/es/app/club-olesa-pat%C3%AD/id6444639283?l=ca") }} >
+              <source src="assets/images/CopApp.mp4" type="video/mp4" />
+            </video>
 
-        <div>
-          <video autoPlay muted loop onClick={() => { goToUrl("http://www.clubolesapati.cat") }} >
-            <source src="assets/images/Shot_01.mp4" type="video/mp4" />
-          </video>
-         
-        </div>
-        <div>
-          <img src="http://clubolesapati.cat//images/dynamic/newsImages/70.jpg" />
-        </div>
-        
-      </Divz>
+          </div>
+          <div>
+          <video autoPlay muted loop onClick={() => { goToUrl("http://clubolesapati.cat/v2") }} >
+              <source src="assets/images/CopWeb.mp4" type="video/mp4" />
+            </video>
+          </div>
+
+        </Divz>
+      </div>
+      {/*  */}
 
     </>
   );
