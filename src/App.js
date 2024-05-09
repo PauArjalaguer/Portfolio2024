95d80406365d7ee4e5ede053a73ede624e958f73
Pau Arjalaguer <pau.arjalaguer@gimage.es>
2024-05-09 15:08:17 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Divz } from "divz";
 function App() {
+  const goToUrl = (url) => {
+    window.open(url, '_blank');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <video autoPlay muted loop id="background-video" className='background'>
+        <source src="assets/images/video.mp4" type="video/mp4" />
+      </video> <div className='flex justify-center mt-20'>
+        <h1 className='font-bold text-slate-700 text-4xl'>Pau Arjalaguer</h1>
+      </div>
+      <Divz autoPlay={true}
+        autoPlayDuration={10000}>
+
+        <div>
+          <video autoPlay muted loop onClick={() => { goToUrl("http://www.clubolesapati.cat") }}>
+            <source src="assets/images/Shot_01.mp4" type="video/mp4" />
+          </video>
+         
+        </div>
+        <div>
+          <img src="http://clubolesapati.cat//images/dynamic/newsImages/70.jpg" />
+        </div>
+        
+      </Divz>
+
+    </>
   );
 }
 
