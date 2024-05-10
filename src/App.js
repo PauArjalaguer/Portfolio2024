871a9bac87e544cee0814514586eaf7c67742af6
Pau Arjalaguer <pau.arjalaguer@gmail.com>
2024-05-10 21:35:53 +0200

    Afegit Light Training

--- src/App.js
+++ src/App.js
@@ -23,28 +23,35 @@ function App() {
           autoPlayDuration={12000}>
           <div>
             <div className='absolute top-0 left-0 flex flex-col w-full   p-6 text-white font-poppins '>
-            <h1 className=' font-poppins  text-slate-700  text-4xl text-white font-bold p-0 m-0'>COP - iOS</h1>&bull; React Native<br />&bull; Backend en Php i MySql<small>Click per descarregar</small></div>
+              <h1 className=' font-poppins  text-slate-700  text-4xl text-white font-bold p-0 m-0'>COP - iOS</h1>&bull; React Native<br />&bull; Backend en Php i MySql<small>Click per descarregar</small></div>
             <video autoPlay muted loop onClick={() => { goToUrl("https://apps.apple.com/es/app/club-olesa-pat%C3%AD/id6444639283?l=ca") }} >
               <source src="assets/images/CopApp.mp4" type="video/mp4" />
             </video>
 
           </div>
-          <div>   
+          <div>
             <div className='absolute top-0 left-0 flex flex-col w-full   p-6 text-white font-poppins '>
-            <h1 className=' font-poppins  text-slate-700  text-4xl text-white font-bold p-0 m-0'>COP - Android</h1>&bull; React Native<br />&bull; Backend en Php i MySql<small>Click per descarregar</small></div>
+              <h1 className=' font-poppins  text-slate-700  text-4xl text-white font-bold p-0 m-0'>COP - Android</h1>&bull; React Native<br />&bull; Backend en Php i MySql<small>Click per descarregar</small></div>
             <video autoPlay muted loop onClick={() => { goToUrl("https://github.com/PauArjalaguer/CopApp2024/releases/download/apk/COPapp2024.apk") }} >
               <source src="assets/images/CopAndroid.mp4" type="video/mp4" />
             </video>
 
           </div>
           <div>
-          <div className='absolute top-0 left-0 flex flex-col w-full   p-6 text-slate-700 font-poppins '>
-            <h1 className=' font-poppins  text-slate-700  text-4xl text-slate-700 font-bold'>COP - Web</h1>&bull; React <br />&bull; Backend en Php i MySql<small>Click per accedir</small></div>
+            <div className='absolute top-0 left-0 flex flex-col w-full   p-6 text-slate-700 font-poppins '>
+              <h1 className=' font-poppins  text-slate-700  text-4xl text-slate-700 font-bold'>COP - Web</h1>&bull; React <br />&bull; Backend en Php i MySql<small>Click per accedir</small></div>
             <video autoPlay muted loop onClick={() => { goToUrl("http://clubolesapati.cat/v2") }} >
               <source src="assets/images/CopWeb.mp4" type="video/mp4" />
             </video>
           </div>
-         {/*  <div>
+          <div>
+            <div className='absolute top-0 left-0 flex flex-col w-full   p-6 text-slate-700 font-poppins '>
+              <h1 className=' font-poppins  text-slate-700  text-4xl text-slate-700 font-bold'>COP - Web</h1>&bull; React <br /><small>Click per accedir</small></div>
+            <video autoPlay muted loop onClick={() => { goToUrl("https://light-training.vercel.app/") }} >
+              <source src="assets/images/lightTraining.mp4" type="video/mp4" />
+            </video>
+          </div>
+          {/*  <div>
           <div className='absolute top-0 left-0 flex flex-col w-full   p-6 text-slate-700 font-poppins '>
             <h1 className=' font-poppins  text-slate-700  text-4xl text-slate-700 font-bold'>CRM vTiger</h1>
             &bull; PHP i MySql </div>
@@ -55,9 +62,9 @@ function App() {
         </Divz>
       </div>
       {/*  */}
-      
+
     </>
-    
+
   );
 }
 
